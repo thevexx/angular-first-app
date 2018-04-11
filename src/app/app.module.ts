d41f99651098f849c76936dbf716829e5241a75d
Chehir Dhawedi <thevexx@users.noreply.github.com>
2018-04-11 15:17:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
